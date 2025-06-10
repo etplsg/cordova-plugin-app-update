9e783a928c09f443933ed8c9b56ce35a5c088a82
etplsg <weiqi@eucharist-tech.com>
2025-06-10 17:03:38 +0800

    Update DownloadHandler.java
    10 Jun 2025 fix auto-update install tested Android 12

--- src/android/DownloadHandler.java
+++ src/android/DownloadHandler.java
@@ -104,8 +104,8 @@ public class DownloadHandler extends Handler {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
             LOG.d(TAG, "Build SDK Greater than or equal to Nougat");
             String applicationId = (String) BuildHelper.getBuildConfigValue((Activity) mContext, "APPLICATION_ID");
-            Uri apkUri = FileProvider.getUriForFile(mContext, mContext.getPackageName() + ".appupdate.provider", apkFile);
-            Intent i = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+            Uri apkUri = FileProvider.getUriForFile(mContext, applicationId + ".fileprovider", apkFile);
+            Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(apkUri);
             i.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             mContext.startActivity(i);
